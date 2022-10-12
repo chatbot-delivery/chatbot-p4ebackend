80beca618175368413be4b2860109838e02135c0
Saroj Gharat <saroj.gharat@effem.com>
2022-10-12 11:25:09 +0200

    Initial commit

---
+++ src/main/java/com/chatbot/notification/service/NotificationService.java
@@ -0,0 +1,26 @@
+package com.chatbot.notification.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.chatbot.notification.service.gateway.UserPreferenceGateway;
+import com.chatbot.notification.service.gateway.WhtsAppNotificationGateway;
+import com.chatbot.notification.service.model.UserProfile;
+
+@Service
+public class NotificationService {
+	@Autowired
+	WhtsAppNotificationGateway whtsAppNotificationGateway;
+	@Autowired
+	UserPreferenceGateway userPreferenceGateway;
+
+	public String  sendWhtsAppNotification(String userId) {
+		UserProfile userProfile = userPreferenceGateway.getUserProfile(userId);
+		String whtsAppNo = String.valueOf(userProfile.getCountry_code()) + String.valueOf(userProfile.getPhone_no());
+		long trackingId = (long) Math.floor(Math.random() * 9_000_000_000L) + 1_000_000_000L;
+		return whtsAppNotificationGateway.sendNotification(whtsAppNo, userProfile.getName(),userProfile.getPreferred_lang(),trackingId);
+	}
+	
+	
+
+}
---
+++ src/main/java/com/chatbot/notification/service/gateway/UserPreferenceGateway.java
@@ -0,0 +1,55 @@
+package com.chatbot.notification.service.gateway;
+
+import org.springframework.stereotype.Component;
+
+import com.chatbot.notification.service.model.UserProfile;
+
+@Component
+public class UserPreferenceGateway {
+
+	public UserProfile getUserProfile(String userId) {
+		
+		UserProfile userProfile;
+		
+		switch(userId) {
+			case "saroj" : 			
+				userProfile = new UserProfile("Saroj", Long.valueOf("626662987"), 31, "en");
+				break;
+			
+			
+			case "jansen" : 			
+				userProfile = new UserProfile("Jansen", Long.valueOf("626662987"), 31, "nl");
+				break;
+			
+			
+			case "amit" : 			
+				userProfile = new UserProfile("Amit", Long.valueOf("9588403341"), 31, "en");
+				break;
+			
+			
+			case "anup" : 			
+				userProfile = new UserProfile("Anup", Long.valueOf("650235394"), 31, "nl");
+				break;
+			
+			
+			case "santosh" : 			
+				userProfile = new UserProfile("Santosh", Long.valueOf("626662987"), 31, "en");
+				break;
+			
+			
+			case "kanchan" : 			
+				userProfile = new UserProfile("Santosh", Long.valueOf("626662987"), 31, "en");
+				break;
+			
+			
+			default : 			
+				userProfile = new UserProfile("Saroj", Long.valueOf("626662987"), 31, "en");
+				break;
+			
+
+		}
+		return userProfile;
+
+	}
+
+}
