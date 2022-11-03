0f83608561d4aa062e3b8c212c655c623ff68d95
Saroj Gharat <saroj.gharat@effem.com>
2022-11-03 06:55:38 +0100

    updated

--- src/main/java/com/chatbot/notification/service/gateway/UserPreferenceGateway.java
+++ src/main/java/com/chatbot/notification/service/gateway/UserPreferenceGateway.java
@@ -16,6 +16,18 @@ public class UserPreferenceGateway {
 				userProfile = new UserProfile("Saroj", Long.valueOf("626662987"), 31, "en-US");
 				break;
 			
+			case "saroj-nl" : 			
+				userProfile = new UserProfile("Saroj", Long.valueOf("626662987"), 31, "nl");
+				break;
+				
+			case "saroj-cn" : 			
+				userProfile = new UserProfile("Saroj", Long.valueOf("626662987"), 31, "zh-CN");
+				break;
+				
+			case "saroj-ar" : 			
+				userProfile = new UserProfile("Saroj", Long.valueOf("626662987"), 31, "ar");
+				break;
+			
 			case "jansen" : 			
 				userProfile = new UserProfile("Jansen", Long.valueOf("626662987"), 31, "nl");
 				break;
@@ -34,11 +46,23 @@ public class UserPreferenceGateway {
 			
 			
 			case "anup" : 			
-				userProfile = new UserProfile("Anup", Long.valueOf("650235394"), 31, "nl-NL");
+				userProfile = new UserProfile("Anup", Long.valueOf("650235394"), 31, "en-US");
 				break;
 			
+			case "anup-nl" : 			
+				userProfile = new UserProfile("Anup", Long.valueOf("650235394"), 31, "nl");
+				break;
+				
+			case "anup-ar" : 			
+				userProfile = new UserProfile("Anup", Long.valueOf("650235394"), 31, "ar");
+				break;
+				
+			case "anup-cn" : 			
+				userProfile = new UserProfile("Anup", Long.valueOf("650235394"), 31, "zh-CN");
+				break;
+				
 			case "santosh" : 			
-				userProfile = new UserProfile("Santosh", Long.valueOf("626662987"), 31, "en-US");
+				userProfile = new UserProfile("Santosh", Long.valueOf("9167202105"), 91, "en-US");
 				break;
 			
 			
